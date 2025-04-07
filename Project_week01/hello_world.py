8a8b3e6ca3fa620c1ebc2fe3d7c1d948d2ccb5ff
Sumiyabazar <Sumiya5391@gmail.com>
2025-04-07 16:12:35 +0800

    add: Fullstack-course-projects

--- Project_week01/hello_world.py
+++ Project_week01/hello_world.py
@@ -1,1 +1,16 @@
 print("Welcome to my project")
+your_name = input ("Whats your name: ") # Hereglegchees ner asuuh 
+print("Hello, " + your_name + " ")     # Hereglegchiin ner hewlwh heseg
+print("Nice to meet you !!!")
+
+
+age = input ("How old are you:")      # Hereglegchees nas asuuh heseg
+
+bodolt = 100 - int(age) + 2025
+print(f"Ta {bodolt} ond 100 nas hurne")
+
+
+
+
+
+
---
+++ Project_week01/setup_documentation.md
@@ -0,0 +1,4 @@
+# hicheel hotsrolttoi bga tul bichlegnuude dahin dahin uzej duurij baij l hiilee kk 
+# yg hiine gesen sanaagaa oruulj chdahgui budilsan ym ih garsan
+# command uuda sain tseejilh heregtei gedgiig oilgoj awlaa 
+# bas git hubluu ga huulahdaa nileed ym bolj bga ywnda saijran gej bodoj bn
